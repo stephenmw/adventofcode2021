26b9631e2c25bbe6e6c2e6ef86e7a0823ce26537
Stephen Weinberg <stephenmweinberg@gmail.com>
2021-12-31 09:48:11 -0500

    Add day 7

---
+++ src/solutions/day07.rs
@@ -0,0 +1,46 @@
+pub fn problem1(input: &str) -> String {
+    let mut nums = parser::parse(input).unwrap().1;
+    nums.sort();
+    let median = nums[nums.len() / 2];
+    let sum_dist: i32 = nums.iter().copied().map(|x| (median - x).abs()).sum();
+    format!("{}", sum_dist)
+}
+
+pub fn problem2(input: &str) -> String {
+    let nums = parser::parse(input).unwrap().1;
+    let max = nums.iter().copied().max().unwrap();
+
+    fn cost(nums: &[i32], i: i32) -> usize {
+        nums.iter()
+            .map(|n| (n - i).abs()) // distance
+            .map(|n| n * (n+1) / 2) // sum of 1 to n
+            .sum::<i32>() as usize
+    }
+
+    let ans = (0..=max).map(|i| cost(&nums, i)).min().unwrap();
+    format!("{}", ans)
+}
+
+mod parser {
+    use crate::lib::combinators::*;
+
+    pub fn parse(input: &str) -> IResult<&str, Vec<i32>> {
+        complete(separated_list1(tag(","), uint))(input)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE_INPUT: &str = "16,1,2,0,4,2,7,1,2,14";
+
+    #[test]
+    fn problem1_test() {
+        assert_eq!(problem1(EXAMPLE_INPUT), "37")
+    }
+    #[test]
+    fn problem2_test() {
+        assert_eq!(problem2(EXAMPLE_INPUT), "168")
+    }
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -5,6 +5,7 @@ mod day02;
 mod day03;
 mod day04;
 mod day06;
+mod day07;
 mod day09;
 mod day10;
 mod day11;
@@ -37,6 +38,7 @@ fn init_solutions() -> HashMap<(usize, usize), Solution> {
     day!(3, day03);
     day!(4, day04);
     day!(6, day06);
+    day!(7, day07);
     day!(9, day09);
     day!(10, day10);
     day!(11, day11);
